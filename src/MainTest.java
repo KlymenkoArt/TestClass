79061ad4055df3ee6484c5e5d7fbd5a8e4380e3e
illuhayacino <56147785+illuhayacino@users.noreply.github.com>
2020-12-17 11:19:53 +0300

    Changing class name

---
+++ src/MainTest.java
@@ -0,0 +1,10 @@
+public class MainTest {
+    public static void main(String[] args) {
+
+    Test mega = new Test();
+    mega.ChangeName("Mega ");
+
+    Test smoke = new Test();
+    smoke.ChangeName("Smoke ");
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,6 @@
+ public class Test {
+
+    public void ChangeName (String name){
+        System.out.println(name + getClass().getName());
+    }
+}
